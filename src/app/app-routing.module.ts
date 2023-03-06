b93b176c8abc8604df904955b53919275859d01c
Cuong Nguyen <nguyen.cuonguuu@gmail.com>
2023-03-06 23:49:10 +0700

    Init project code base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+
+const routes: Routes = [
+  {
+    path: '', component: AppLayoutComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/layout/app.topbar.component.ts
@@ -0,0 +1,53 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { LayoutService } from "./service/app.layout.service";
+
+@Component({
+    selector: 'app-topbar',
+    templateUrl: './app.topbar.component.html'
+})
+export class AppTopBarComponent {
+
+    menu: MenuItem[] = [];
+
+    @ViewChild('searchinput') searchInput!: ElementRef;
+
+    @ViewChild('menubutton') menuButton!: ElementRef;
+
+    searchActive: boolean = false;
+
+    constructor(public layoutService: LayoutService) {}
+
+    onMenuButtonClick() {
+        this.layoutService.onMenuToggle();
+    }
+
+    activateSearch() {
+        this.searchActive = true;
+        setTimeout(() => {
+            this.searchInput.nativeElement.focus();
+        }, 100);
+    }
+
+    deactivateSearch() {
+        this.searchActive = false;
+    }
+
+    removeTab(event: MouseEvent, item: MenuItem, index: number) {
+        this.layoutService.onTabClose(item, index);
+        event.preventDefault();
+    }
+
+    get layoutTheme(): string {
+        return this.layoutService.config.layoutTheme;
+    }
+
+    get colorScheme(): string {
+        return this.layoutService.config.colorScheme;
+    }
+
+
+    get tabs(): MenuItem[] {
+        return this.layoutService.tabs;
+    }
+}
