6ea4e83a86c8b894ccefa25020db691fae782346
Cuong Nguyen <nguyen.cuonguuu@gmail.com>
2023-03-12 02:38:03 +0700

    chore: add form validation

--- src/app/components/dashboard/components/employee/employee.component.ts
+++ src/app/components/dashboard/components/employee/employee.component.ts
@@ -1,140 +1,137 @@
 import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from "../../services";
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { format } from 'date-fns'
-import { ConfirmationService, MessageService } from "primeng/api";
-import { takeUntil } from "rxjs";
-import { UnSubscribable } from "@shared/directives";
-import { GenderConstant } from "@shared/constants";
-
+import { EmployeeService } from '../../services';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { format } from 'date-fns';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { takeUntil } from 'rxjs';
+import { UnSubscribable } from '@shared/directives';
+import { GenderConstant } from '@shared/constants';
 
 @Component({
-  selector: 'app-table',
-  templateUrl: 'employee.component.html',
-  styleUrls: ['employee.component.scss']
+    selector: 'app-table',
+    templateUrl: 'employee.component.html',
+    styleUrls: ['employee.component.scss']
 })
 export class EmployeeComponent extends UnSubscribable implements OnInit {
+    readonly gender = GenderConstant;
+    selectedGender = 1;
+    employeeData: any;
+    employee: any;
+    employeeDialog!: boolean;
+    employeeCreationDialog!: boolean;
 
-  readonly gender = GenderConstant;
-  selectedGender = 1;
-  employeeData: any;
-  employee: any;
-  employeeDialog!: boolean;
-  employeeCreationDialog!: boolean;
+    submitted!: boolean;
+    isLoading!: boolean;
+    employeeForm!: FormGroup;
 
-  submitted!: boolean;
-  isLoading!: boolean;
-  employeeForm!: FormGroup;
-
-  constructor(
-    private readonly employeeService: EmployeeService,
-    private readonly formBuilder: FormBuilder,
-    private readonly messageService: MessageService,
-    private readonly confirmationService: ConfirmationService
-  ) {
-    super();
-  }
-
-  ngOnInit(): void {
-    this.initForm();
-    this.employeeService.getEmployeeData().subscribe({
-      next: (data) => {
-        this.employeeData = data;
-        this.isLoading = false;
-      },
-      error: (err) => {
-        this.isLoading = false;
-      }
-    })
-  }
-
-  addEmployee(): void {
-    this.employee = {};
-    this.employeeCreationDialog = true
-  }
-
-  editEmployee(employee: any): void {
-    this.employeeDialog = true;
-    this.employee = {
-      ...employee,
-      birthday: new Date(employee.birthday)
-    };
-    if (employee.assessments.length === 0) {
-      let assessmentsObj = {
-        content: ''
-      }
-      employee.assessments.push(assessmentsObj)
+    constructor(
+        private readonly employeeService: EmployeeService,
+        private readonly formBuilder: FormBuilder,
+        private readonly messageService: MessageService,
+        private readonly confirmationService: ConfirmationService
+    ) {
+        super();
     }
-    console.log(employee)
-  }
 
-  saveAddEmployee(): void {
-    this.employeeCreationDialog = false;
-    console.log(this.employeeForm)
-  }
-
-  saveEditEmployee(): void {
-    this.employeeDialog = false;
-    let formData = new FormData();
-    formData.append('FullName', this.employeeForm.get('fullName')?.value)
-    formData.append('PhoneNo', this.employeeForm.get('phoneNo')?.value)
-    formData.append('Email', this.employeeForm.get('email')?.value)
-    formData.append('Address', this.employeeForm.get('address')?.value)
-    formData.append('CCCD', this.employeeForm.get('cccd')?.value)
-    formData.append('Birthday', format(new Date(this.employee.birthday), 'dd-MM-yyyy'))
-    formData.append('ID', this.employee.id)
-    console.log(formData)
-    this.employeeService.editEmployee(formData)
-      .pipe(takeUntil(this.unsubscribeAll))
-      .subscribe({
-        next: (res: any) => {
-          console.log(res)
-        },
-        error: (err: any) => {
-          console.log(err)
-          this.messageService.add({severity: 'error', summary: 'Lỗi', detail: 'Error'});
-        }
-      })
-
-  }
-
-  deleteEmployee(employee: any): void {
-    this.confirmationService.confirm({
-      message: 'Xác nhận xóa nhân viên này',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.employeeService.deleteEmployee(employee.id)
-          .pipe(takeUntil(this.unsubscribeAll))
-          .subscribe({
-            next: (res) => {
-              this.employeeData.splice(this.employeeData.indexOf(employee), 1)
-              this.messageService.add({severity: 'success', detail: 'Xóa thành công', life: 3000});
+    ngOnInit(): void {
+        this.initForm();
+        this.employeeService.getEmployeeData().subscribe({
+            next: (data) => {
+                this.employeeData = data;
+                this.isLoading = false;
             },
-            error: (err: any) => {
-              this.messageService.add({severity: 'error', summary: 'Lỗi', detail: 'Error'});
+            error: (err) => {
+                this.isLoading = false;
             }
-          })
-      }
-    })
-  }
+        });
+    }
 
-  initForm(): void {
-    this.employeeForm = this.formBuilder.group({
-      fullName: [null],
-      phoneNo: [null],
-      cccd: [null],
-      email: [null],
-      address: [null],
-      gender: [null],
-      birthday: [null],
-      assessment: [null]
-    })
-  }
+    addEmployee(): void {
+        this.employee = {};
+        this.employeeCreationDialog = true;
+    }
 
-  hideDialog(): void {
-    this.employeeDialog = false;
-    this.submitted = false;
-  }
+    editEmployee(employee: any): void {
+        this.employeeDialog = true;
+        this.employee = {
+            ...employee,
+            birthday: new Date(employee.birthday)
+        };
+        if (employee.assessments.length === 0) {
+            let assessmentsObj = {
+                content: ''
+            };
+            employee.assessments.push(assessmentsObj);
+        }
+        console.log(employee);
+    }
 
+    saveAddEmployee(): void {
+        this.employeeCreationDialog = false;
+        console.log(this.employeeForm);
+    }
 
+    saveEditEmployee(): void {
+        this.employeeDialog = false;
+        let formData = new FormData();
+        formData.append('FullName', this.employeeForm.get('fullName')?.value);
+        formData.append('PhoneNo', this.employeeForm.get('phoneNo')?.value);
+        formData.append('Email', this.employeeForm.get('email')?.value);
+        formData.append('Address', this.employeeForm.get('address')?.value);
+        formData.append('CCCD', this.employeeForm.get('cccd')?.value);
+        formData.append('Birthday', format(new Date(this.employee.birthday), 'dd-MM-yyyy'));
+        formData.append('ID', this.employee.id);
+        console.log(formData);
+        this.employeeService
+            .editEmployee(formData)
+            .pipe(takeUntil(this.unsubscribeAll))
+            .subscribe({
+                next: (res: any) => {
+                    console.log(res);
+                },
+                error: (err: any) => {
+                    console.log(err);
+                    this.messageService.add({ severity: 'error', summary: 'Lỗi', detail: 'Error' });
+                }
+            });
+    }
+
+    deleteEmployee(employee: any): void {
+        this.confirmationService.confirm({
+            message: 'Xác nhận xóa nhân viên này',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.employeeService
+                    .deleteEmployee(employee.id)
+                    .pipe(takeUntil(this.unsubscribeAll))
+                    .subscribe({
+                        next: (res) => {
+                            this.employeeData.splice(this.employeeData.indexOf(employee), 1);
+                            this.messageService.add({ severity: 'success', detail: 'Xóa thành công', life: 3000 });
+                        },
+                        error: (err: any) => {
+                            this.messageService.add({ severity: 'error', summary: 'Lỗi', detail: 'Error' });
+                        }
+                    });
+            }
+        });
+    }
+
+    initForm(): void {
+        this.employeeForm = this.formBuilder.group({
+            fullName: [null, Validators.required],
+            phoneNo: [null, Validators.required],
+            cccd: [null, Validators.required],
+            email: [null, Validators.required],
+            address: [null, Validators.required],
+            gender: [null, Validators.required],
+            birthday: [null],
+            assessment: [null]
+        });
+    }
+
+    hideDialog(): void {
+        this.employeeDialog = false;
+        this.submitted = false;
+    }
 }
--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,46 +1,47 @@
 import { NgModule } from '@angular/core';
-import { EmployeeService } from "./services";
-import { MessageService } from "primeng/api";
-import { ButtonModule } from "primeng/button";
-import { InputTextModule } from "primeng/inputtext";
-import { ToastModule } from "primeng/toast";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { TableModule } from "primeng/table";
-import { DatePipe } from "@angular/common";
-import { DialogModule } from "primeng/dialog";
-import { ConfirmDialogModule } from "primeng/confirmdialog";
-import { RippleModule } from "primeng/ripple";
-import { MessagesModule } from "primeng/messages";
-import { CalendarModule } from "primeng/calendar";
-import { EmployeeComponent } from "./components/employee";
-import { AccountComponent } from "./components/account";
-import { DashboardRoutingModule } from "./dashboard-routing.module";
-import { AccountService } from "./services/account.service";
-import { GenderPipe } from "@shared/pipes";
-import { DropdownModule } from "primeng/dropdown";
-
+import { EmployeeService } from './services';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { DatePipe, NgClass, NgIf } from '@angular/common';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { RippleModule } from 'primeng/ripple';
+import { MessagesModule } from 'primeng/messages';
+import { CalendarModule } from 'primeng/calendar';
+import { EmployeeComponent } from './components/employee';
+import { AccountComponent } from './components/account';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { AccountService } from './services/account.service';
+import { GenderPipe } from '@shared/pipes';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
-  declarations: [EmployeeComponent, AccountComponent, GenderPipe],
-  imports: [
-    DashboardRoutingModule,
-    TableModule,
-    InputTextModule,
-    ButtonModule,
-    RippleModule,
-    DialogModule,
-    ConfirmDialogModule,
-    ReactiveFormsModule,
-    DatePipe,
-    CalendarModule,
-    FormsModule,
-    MessagesModule,
-    ToastModule,
-    DropdownModule
-  ],
-  exports: [
-    EmployeeComponent, AccountComponent
-  ],
-  providers: [EmployeeService, MessageService, AccountService]
+    declarations: [EmployeeComponent, AccountComponent, GenderPipe],
+    imports: [
+        DashboardRoutingModule,
+        TableModule,
+        InputTextModule,
+        ButtonModule,
+        RippleModule,
+        DialogModule,
+        ConfirmDialogModule,
+        ReactiveFormsModule,
+        DatePipe,
+        CalendarModule,
+        FormsModule,
+        MessagesModule,
+        ToastModule,
+        DropdownModule,
+        NgClass,
+        NgIf,
+        InputNumberModule
+    ],
+    exports: [EmployeeComponent, AccountComponent],
+    providers: [EmployeeService, MessageService, AccountService]
 })
-export class DashboardModule { }
+export class DashboardModule {}
