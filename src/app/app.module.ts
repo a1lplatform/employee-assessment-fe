fa9b5650922123f754c6af69bcbacbee00f7dfab
Cuong Nguyen <nguyen.cuonguuu@gmail.com>
2023-03-15 17:07:39 +0700

    hotifx: routing (eager loading)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { ConfirmationService } from 'primeng/api';
+import { AuthModule } from './components/auth';
+import { DashboardModule } from './components/dashboard/dashboard.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, AppLayoutModule],
+    imports: [BrowserModule, AppRoutingModule, AppLayoutModule, AuthModule, DashboardModule],
     providers: [ConfirmationService],
     bootstrap: [AppComponent]
 })
